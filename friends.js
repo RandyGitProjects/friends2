98653a45f35a2a57f1653e02e969542e3f097b24
Randy Cisneros <rlcisneros90@gmail.com>
2022-12-15 17:21:11 -0700

    Add more greetings

--- friends.js
+++ friends.js
@@ -1,6 +1,9 @@
 var name = "Maggi"
+var name1 = "Sully"
 if (name === "Maggie"){
     console.log(`Hello ${name}`);
+}   else if (name1 === "Sully") {
+    console.log(`Hello ${name1}`); 
 }   else {
     console.log("Hello, there.");
 }
